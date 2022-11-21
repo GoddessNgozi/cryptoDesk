a2415c8309610a048325b72b1210d218668ce4a2
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-21 21:55:13 +0100

    Fetch, filter and display  coins from API

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Coin from './Coin';
 import Search from './Search';
+import { fetchCoins } from '../redux/coins/coins';
 
 const Coins = () => {
+  const coins = useSelector((state) => state.coins);
   const [search, setSearch] = useState('');
+
+  const filteredCoins = coins.filter((coin) => (
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  ));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch]);
 
   const onSearch = (e) => {
     setSearch(e.target.value);
@@ -13,15 +26,17 @@ const Coins = () => {
     <>
       <Search search={search} onSearch={onSearch} />
       <ul className="coins">
-        <Coin
-          key={coin.id}
-          img={coin.icon}
-          id={coin.id}
-          symbol={coin.symbol}
-          rank={coin.rank}
-          name={coin.name}
-          price={coin.price}
-        />
+        { filteredCoins.map((coin) => (
+          <Coin
+            key={coin.id}
+            img={coin.icon}
+            id={coin.id}
+            symbol={coin.symbol}
+            rank={coin.rank}
+            name={coin.name}
+            price={coin.price}
+          />
+        ))}
       </ul>
     </>
   );

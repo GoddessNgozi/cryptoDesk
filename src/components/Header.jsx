b54290c783bfa3cd2ede409ad6396214afcaf4f1
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-22 06:58:44 +0100

    Rename h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => (
             </Link>
           </li>
           <li className="headerList">
-            <h1>Crypto Metrics</h1>
+            <h1>Crypto Desk</h1>
           </li>
           <li className="headerList">
             <div className="icons-class">
